f1d9a59b12aeaf8932c7fc02c0c13a5dbd076966
altilunium <70379302+altilunium@users.noreply.github.com>
2021-04-25 00:15:31 +0700

    Add files via upload

--- carjaw-backup.js
+++ carjaw-backup.js
@@ -37,7 +37,6 @@ copietxt.execCommand("Copy");
 
 var car;
 
-
 function transcrire() {
 car = document.conversion.saisie.value;
 car = car.replace(/’/g, "\'");
@@ -47,85 +46,61 @@ car = car.replace(/t/g, "ت");;
 car = car.replace(/تs/g, "ث");
 car = car.replace(/ṯ/g, "ث");
 car = car.replace(/[jǧ]/g, "ج");
-
+car = car.replace(/ج'/g, "چ");
 car = car.replace(/c/g, "چ");
 car = car.replace(/[HḥḤ]/g, "ح");
 //car = car.replace(/ح'/g, "خ");
-car = car.replace(/k/g, "ک");
-car = car.replace(/کh/g, "خ");
+car = car.replace(/kh/g, "خ");
 car = car.replace(/[xẖK]/g, "خ");
 car = car.replace(/d/g, "د");
-car = car.replace(/دz/g, "ذ");
+car = car.replace(/د'/g, "ذ");
 car = car.replace(/ḏ/g, "ذ");
 car = car.replace(/r/g, "ر");
-//car = car.replace(/ر'/g, "ز");
+car = car.replace(/ر'/g, "ز");
 car = car.replace(/z/g, "ز");
 car = car.replace(/s/g, "س");
-car = car.replace(/سy/g, "ش");
-//car = car.replace(/š/g, "ش");
-car = car.replace(/سh/g, "ص");
-//car = car.replace(/ص'/g, "ض");
-car = car.replace(/دh/g, "ض");
-car = car.replace(/تh/g, "ط");
-//car = car.replace(/ط'/g, "ظ");
-car = car.replace(/زh/g, "ظ");
-car = car.replace(/'/g, "ع");
-car = car.replace(/n/g, "ن");
-car = car.replace(/نg/g, "ڠ");
-car = car.replace(/g/g, "ݢ");
-car = car.replace(/ݢh/g, "غ");
-//car = car.replace(/ġ/g, "غ");
-
-//car = car.replace(/N/g, "ڠ");
+car = car.replace(/س'/g, "ش");
+car = car.replace(/š/g, "ش");
+car = car.replace(/[Sṣ]/g, "ص");
+car = car.replace(/ص'/g, "ض");
+car = car.replace(/[Dḍ]/g, "ض");
+car = car.replace(/[Tṭ]/g, "ط");
+car = car.replace(/ط'/g, "ظ");
+car = car.replace(/[Zẓ]/g, "ظ");
+car = car.replace(/[gʿ]/g, "ع");
+car = car.replace(/ع'/g, "غ");
+car = car.replace(/ġ/g, "غ");
+car = car.replace(/غ'/g, "ڠ");
+car = car.replace(/N/g, "ڠ");
 car = car.replace(/f/g, "ف");
 car = car.replace(/ف'/g, "ڤ");
 car = car.replace(/p/g, "ڤ");
 car = car.replace(/q/g, "ق");
 car = car.replace(/ق'/g, "ڨ");
-
-
+car = car.replace(/k/g, "ک");
+car = car.replace(/ک'/g, "ݢ");
 car = car.replace(/l/g, "ل");
 car = car.replace(/m/g, "م");
-
-car = car.replace(/نy/g, "ڽ");
-//car = car.replace(/ñ/g, "ڽ");
+car = car.replace(/n/g, "ن");
+car = car.replace(/ن'/g, "ڽ");
+car = car.replace(/ñ/g, "ڽ");
 car = car.replace(/h/g, "ه");
-car = car.replace(/ه\./g, "ة");
+car = car.replace(/ه'/g, "ة");
 car = car.replace(/[wouôûōū]/g, "و");
 car = car.replace(/v/g, "ۏ");
-//car = car.replace(/و'/g, "ۏ");
+car = car.replace(/و'/g, "ۏ");
 car = car.replace(/[yiîī]/g, "ي");
 car = car.replace(/e/g, "ى");
-car = car.replace(/ء-/g, "ئ");
 car = car.replace(/-/g, "ء");
-
 car = car.replace(/ʾ/g, "ء");
 
 car = car.replace(/اءء/g, "إ");
-car = car.replace(/[AIE]/g, "أ");
-car = car.replace(/أ\./g, "إ");
-
-
+car = car.replace(/[AIE]/g, "إ");
 
 car = car.replace(/_/g, "ـ");
 car = car.replace(/\?/g, "؟");
 car = car.replace(/\;/g, "؛");
 car = car.replace(/\,/g, "،");
-
-
-car = car.replace(/ث\./g,"تس");
-car = car.replace(/خ\./g,"که");
-car = car.replace(/ذ\./g,"دز");
-car = car.replace(/ش\./g,"سي");
-car = car.replace(/ص\./g,"سه");
-car = car.replace(/ض\./g,"ده");
-car = car.replace(/ط\./g,"ته");
-car = car.replace(/ظ\./g,"زه");
-car = car.replace(/غ\./g,"ݢه");
-car = car.replace(/ڠ\./g,"نݢ");
-car = car.replace(/ڽ\./g,"ني");
-
-
 
 startPos = document.conversion.saisie.selectionStart;
 endPos = document.conversion.saisie.selectionEnd;
