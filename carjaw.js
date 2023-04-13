06c8829b744331c24aea6df3f73717d0c9b28ea4
Altilunium <70379302+altilunium@users.noreply.github.com>
2023-04-13 15:05:05 +0700

    Bugfix

--- carjaw.js
+++ carjaw.js
@@ -45,55 +45,36 @@ car = car.replace(/[aâàā]/g, "ا");
 car = car.replace(/b/g, "ب");
 car = car.replace(/t/g, "ت");;
 car = car.replace(/تs/g, "ث");
-car = car.replace(/ṯ/g, "ث");
-car = car.replace(/[jǧ]/g, "ج");
-
+car = car.replace(/j/g, "ج");
 car = car.replace(/c/g, "چ");
-car = car.replace(/[HḥḤ]/g, "ح");
-//car = car.replace(/ح'/g, "خ");
+car = car.replace(/H/g, "ح");
 car = car.replace(/k/g, "ک");
 car = car.replace(/کh/g, "خ");
-car = car.replace(/[xẖK]/g, "خ");
 car = car.replace(/d/g, "د");
 car = car.replace(/دz/g, "ذ");
-car = car.replace(/ḏ/g, "ذ");
 car = car.replace(/r/g, "ر");
-//car = car.replace(/ر'/g, "ز");
 car = car.replace(/z/g, "ز");
 car = car.replace(/s/g, "س");
 car = car.replace(/سy/g, "ش");
-//car = car.replace(/š/g, "ش");
 car = car.replace(/سh/g, "ص");
-//car = car.replace(/ص'/g, "ض");
 car = car.replace(/دh/g, "ض");
 car = car.replace(/تh/g, "ط");
-//car = car.replace(/ط'/g, "ظ");
 car = car.replace(/زh/g, "ظ");
 car = car.replace(/'/g, "ع");
 car = car.replace(/n/g, "ن");
 car = car.replace(/نg/g, "ڠ");
 car = car.replace(/g/g, "ݢ");
 car = car.replace(/ݢh/g, "غ");
-//car = car.replace(/ġ/g, "غ");
-
-//car = car.replace(/N/g, "ڠ");
 car = car.replace(/f/g, "ف");
-car = car.replace(/ف'/g, "ڤ");
 car = car.replace(/p/g, "ڤ");
 car = car.replace(/q/g, "ق");
-car = car.replace(/ق'/g, "ڨ");
-
-
 car = car.replace(/l/g, "ل");
 car = car.replace(/m/g, "م");
-
 car = car.replace(/نy/g, "ڽ");
-//car = car.replace(/ñ/g, "ڽ");
 car = car.replace(/h/g, "ه");
 car = car.replace(/ه\./g, "ة");
 car = car.replace(/[wouôûōū]/g, "و");
 car = car.replace(/v/g, "ۏ");
-//car = car.replace(/و'/g, "ۏ");
 car = car.replace(/[yiîī]/g, "ي");
 car = car.replace(/e/g, "ى");
 car = car.replace(/ء-/g, "ئ");
@@ -126,6 +107,7 @@ car = car.replace(/ظ\./g,"زه");
 car = car.replace(/غ\./g,"ݢه");
 car = car.replace(/ڠ\./g,"نݢ");
 car = car.replace(/ڽ\./g,"ني");
+car = car.replace(/وء/g,"ؤ");
 
 car = car.replace(/1/g,'\u064e')
 car = car.replace(/\u064e\./g,'\u064b')
